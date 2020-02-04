99f435fe1645c8580cf9e5e430b6133bb1f49ce2
Ephriam Henderson <hendersonephriamn@gmail.com>
2020-02-04 14:05:25 -0600

    Initial Server Setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const { ApolloServer, gql } = require("apollo-server");
+const fs = require("fs");
+
+const schema = gql(fs.readFileSync("graphql/schema.graphql", "utf8"));
+
+server = new ApolloServer({
+	typeDefs: schema
+});
+
+server
+	.listen({ port: process.env.PORT || 4000, tracing: true })
+	.then(({ url }) => {
+		console.log(`Server Started at ${url}`);
+	});
